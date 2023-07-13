6566ac2e4a6250a16aa62670eb3aa2c259bc6c1d
TheMadTomato1209 <96842991+TheMadTomato1209@users.noreply.github.com>
2023-07-13 15:13:57 +0300

    Пъровоначално качване на проект

---
+++ Account.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+
+
+namespace ConsoleApp79
+{  
+    [Serializable]
+    class Account
+    {
+        public Account() {
+            string uName;
+            Console.Write("Enter username: ");
+            uName = Console.ReadLine();
+            while (true) {
+                if (Regex.IsMatch(uName, @"\A[\w ]+\z")) {
+                    if (!File.Exists($"Accounts\\{uName}.dll")) break;
+                    Console.Write("Account name already taken, try a different one: ");
+                }
+                else Console.WriteLine("Username can only contain letters, underscores and spaces!");
+                
+                uName = Console.ReadLine();
+            }
+            //a Console.WriteLine("sukes");
+            userName = uName;
+            Console.WriteLine("Password must be between 8 and 16 characters and must contain letters and numbers!");
+            Console.Write("Enter password: ");
+            string password = Program.HiddenString();
+            Console.WriteLine();
+            while (true)
+            {
+                if (Regex.IsMatch(password, @"\A(?=.*[0-9]+)(?=.*\w+).{8,16}\z")) break;
+                Console.WriteLine("password must be between 8 and 16 characters and must contain letters and numbers!");
+                Console.Write("Enter password: ");
+                password = Program.HiddenString();
+                Console.WriteLine();
+            }
+            Console.Write("Repeat password: ");
+            string repeatPassword = Program.HiddenString();
+            Console.WriteLine();
+            while (true){
+                if (repeatPassword == "!") {
+                    password = Program.HiddenString();
+                    Console.WriteLine();
+                    Console.Write("Repeat password: ");
+                    repeatPassword = Program.HiddenString();
+                    Console.WriteLine();
+                    continue;
+                }
+                else if (password == repeatPassword) break;
+                Console.Write("Passwords don't match. Try again ot enter \"!\" to enter a different password: ");
+                repeatPassword = Program.HiddenString();
+                Console.WriteLine();
+            }
+
+            byte[] salt = new byte[16];
+            new RNGCryptoServiceProvider().GetBytes(salt);
+            var pdfk = new Rfc2898DeriveBytes(password, salt, 100000);
+            byte[] hash = pdfk.GetBytes(20);
+            byte[] hashSalt = new byte[36];
+            Array.Copy(hash, 0, hashSalt, 0, 20);
+            Array.Copy(salt, 0, hashSalt, 20, 16);
+            PasswordHash = Convert.ToBase64String(hashSalt);
+            // Console.WriteLine(PasswordHash);
+
+            
+            while (true)
+            {
+                Console.WriteLine("Enter the information which you want to save:");
+                string dataIn = Console.ReadLine();
+                data = Program.EnDecrypt(dataIn, password);
+                Console.WriteLine();
+                Console.WriteLine(string.Join("", Program.EnDecrypt(string.Join("", data), password)));
+
+                Console.WriteLine("Do you wish to continue?");
+                string cont = Console.ReadLine();
+                while (true)
+                {
+                    if (Regex.IsMatch(cont, @"\A(y|Y|yes|Yes|YES)\z")) goto InfoDone;
+                    else if (Regex.IsMatch(cont, @"\A(n|N|no|No|NO)\z")) break;
+                    Console.WriteLine("Enter yes or no");
+                    cont = Console.ReadLine();
+                }
+                
+            }
+
+            InfoDone:
+            Console.Write(new string(' ', 21));
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.WriteLine("\nInformation saved successfully!");
+            Console.ReadKey();
+        }
+        public string userName;
+        string passwordHash;
+        public string[] data { get; set; }
+
+        public string PasswordHash { get; protected set; }
+
+        public override string ToString()
+        {
+            return userName + PasswordHash;
+        }
+        public string RetrieveData() {
+            return string.Join("", data);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace ConsoleApp79
+{
+    class Program
+    {
+        public static string HiddenString() {
+            StringBuilder stringBuilder = new StringBuilder();
+            while (true)
+            {
+                ConsoleKeyInfo keyI = Console.ReadKey(true);
+                char keyChar = keyI.KeyChar;
+                //Console.WriteLine((int)keyChar);
+                if (keyChar >= 32 && keyChar <= 126)
+                {
+                    Console.Write('*');
+                    stringBuilder.Append(keyChar);
+                }
+                else if (keyI.Key == ConsoleKey.Enter) return stringBuilder.ToString();
+                else if (keyI.Key == ConsoleKey.Backspace && Console.CursorLeft != 0) {
+                    Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                    Console.Write(' ');
+                    Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                    stringBuilder.Remove(stringBuilder.Length - 1, 1);
+                }
+            }
+        }
+        public static bool PasswordThings(Account account, string password) {
+            byte[] savedPasswordHash = Convert.FromBase64String(account.PasswordHash);
+            byte[] salt = new byte[16];
+            Array.Copy(savedPasswordHash, 20, salt, 0, 16);
+
+            var pdkf2 = new Rfc2898DeriveBytes(password, salt, 100000);
+            byte[] newPasswordHash = pdkf2.GetBytes(20);
+
+            bool match = true;
+            for (int i = 0; i < 20; i++)
+            {
+                if (savedPasswordHash[i] != newPasswordHash[i]) {
+                    match = false;
+                    break;
+                }
+            }
+            return match;
+        }
+        public static string[] EnDecrypt(string input, string key) {
+            string[] inBinary = new string[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                inBinary[i] = Convert.ToString((int)input[i], 2);
+                if (inBinary[i].Length < 7)
+                {
+                    StringBuilder sB = new StringBuilder();
+                    sB.Append(inBinary[i]);
+                    for (int j = inBinary[i].Length; j < 7; j++)
+                    {
+                        // Console.WriteLine(sB + "alo");
+                        sB.Insert(0, "0");
+                    }
+                    // Console.WriteLine(sB + "ale");
+                    inBinary[i] = sB.ToString();
+                }
+                //a Console.Write(inBinary[i] + " ");
+            }
+            //a Console.WriteLine();
+
+            string[] keyBinary = new string[key.Length];
+            for (int i = 0; i < key.Length; i++)
+            {
+                keyBinary[i] = Convert.ToString((int)key[i], 2);
+                if (keyBinary[i].Length < 7)
+                {
+                    StringBuilder sB = new StringBuilder();
+                    sB.Append(keyBinary[i]);
+                    for (int j = keyBinary[i].Length; j < 7; j++)
+                    {
+                        // Console.WriteLine(sB + "alo");
+                        sB.Insert(0, "0");
+                    }
+                    // Console.WriteLine(sB + "ale");
+                    keyBinary[i] = sB.ToString();
+                }
+                //a Console.Write(keyBinary[i] + " ");
+            }
+
+            //a Console.WriteLine();
+            string[] encryptedBinary = new string[inBinary.Length];
+            for (int i = 0; i < inBinary.Length; i++)
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int j = 0; j < inBinary[i].Length; j++)
+                {
+                    int ki = i;
+                    while (ki >= key.Length) ki -= key.Length;
+                    if (int.Parse(inBinary[i][j].ToString()) + int.Parse(keyBinary[ki][j].ToString()) == 1) stringBuilder.Append("1");
+                    else stringBuilder.Append("0");
+                }
+                //Console.Write(stringBuilder.ToString() + " ");
+                encryptedBinary[i] = ((char)Convert.ToInt32(stringBuilder.ToString(), 2)).ToString();
+                //a Console.Write(encryptedBinary[i]);
+
+            }
+            return encryptedBinary;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HiddenString());
+            Console.ReadKey();
+            Menu:
+            Console.Clear();
+            while (true)
+            {
+                Console.WriteLine("Welcome to Kryptor!");
+                Console.WriteLine("1. Create account");
+                Console.WriteLine("2. Log in");
+                Console.WriteLine("3. Exit");
+                Console.WriteLine("Enter the number of your choice:");
+                string choice = Console.ReadLine();
+                while (true)
+                {
+                    if (choice == "1")
+                    {
+                        Console.Write(new string(' ', 21));
+                        Console.SetCursorPosition(0, Console.CursorTop);
+                        Account account = new Account();
+                        if (!Directory.Exists("Accounts")) Directory.CreateDirectory("Accounts");
+                        Stream stream = new FileStream($"Accounts\\{account.userName}.dll", FileMode.Create, FileAccess.Write);
+                        IFormatter formatter = new BinaryFormatter();
+                        formatter.Serialize(stream, account);
+                        stream.Close();
+                        goto Menu;
+                    }
+                    else if (choice == "2")
+                    {
+                        Console.Write(new string(' ', 21));
+                        Console.SetCursorPosition(0, Console.CursorTop);
+                        Console.WriteLine("yahu");
+
+                        Console.Write("Enter Username: ");
+                        string uName = Console.ReadLine();
+
+                        while (true)
+                        {
+                            if (Regex.IsMatch(uName, @"\A[\w ]+\z"))
+                            {
+                                if (File.Exists($"Accounts\\{uName}.dll")) break;
+                                Console.Write("Account not found, try again: ");
+                            }
+                            else Console.WriteLine("Username can only contain letters, underscores and spaces!");
+
+                            uName = Console.ReadLine();
+                        }
+
+                        Stream stream = new FileStream($"Accounts\\{uName}.dll", FileMode.Open, FileAccess.Read);
+                        IFormatter formatter = new BinaryFormatter();
+                        Account getAccount = (Account)formatter.Deserialize(stream);
+                        stream.Close();
+                        Console.Write("Enter password: ");
+                        string password = HiddenString();
+                        Console.WriteLine();
+                        while (true)
+                        {
+                            if (PasswordThings(getAccount, password)) break;
+                            Console.WriteLine("Wrong password!");
+                            Console.Write("Enter password: ");
+                            password = HiddenString();
+                            Console.WriteLine();
+                        }
+                        Console.WriteLine("Successfully logged in! Do you wish to see your information?");
+                        string cont = Console.ReadLine();
+                        while (true)
+                        {
+                            if (Regex.IsMatch(cont, @"\A(y|Y|yes|Yes|YES)\z")) break;
+                            else if (Regex.IsMatch(cont, @"\A(n|N|no|No|NO)\z")) goto Menu;
+                            Console.WriteLine("Enter yes or no");
+                            cont = Console.ReadLine();
+                        }
+
+                        Console.WriteLine(string.Join("", EnDecrypt(getAccount.RetrieveData(), password)));
+
+                        Console.WriteLine("Do you wish to save new information?");
+                        cont = Console.ReadLine();
+                        while (true)
+                        {
+                            if (Regex.IsMatch(cont, @"\A(y|Y|yes|Yes|YES)\z")) break;
+                            else if (Regex.IsMatch(cont, @"\A(n|N|no|No|NO)\z"))
+                            {
+                                Console.WriteLine("Logging out...");
+                                Thread.Sleep(1000);
+                                goto Menu;
+                            }
+                            Console.WriteLine("Enter yes or no");
+                            cont = Console.ReadLine();
+                        }
+
+                        Console.WriteLine("Enter the information which you want to save:");
+                        string dataIn = Console.ReadLine();
+                        getAccount.data = Program.EnDecrypt(dataIn, password);
+                        Console.WriteLine();
+                        Console.WriteLine(string.Join("", Program.EnDecrypt(string.Join("", getAccount.data), password)));
+
+                        Console.WriteLine("Do you wish to continue?");
+                        cont = Console.ReadLine();
+                        while (true)
+                        {
+                            if (Regex.IsMatch(cont, @"\A(y|Y|yes|Yes|YES)\z")) goto InfoDone;
+                            else if (Regex.IsMatch(cont, @"\A(n|N|no|No|NO)\z")) break;
+                            Console.WriteLine("Enter yes or no");
+                            cont = Console.ReadLine();
+                        }
+
+                        InfoDone:
+                        Console.Write(new string(' ', 21));
+                        Console.SetCursorPosition(0, Console.CursorTop);
+                        Console.WriteLine("\nInformation saved successfully!");
+                        Console.ReadKey();
+
+                        stream = new FileStream($"Accounts\\{getAccount.userName}.dll", FileMode.Create, FileAccess.Write);
+                        formatter = new BinaryFormatter();
+                        formatter.Serialize(stream, getAccount);
+                        stream.Close();
+                        goto Menu;
+                    }
+                    else if (choice == "3")
+                    {
+                        Console.WriteLine("Thank you for using Kryptor!");
+                        Environment.Exit(0);
+                    }
+                    Console.Write("Enter a valid number!");
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);
+                    Console.Write(new string(' ', choice.Length));
+                    Console.SetCursorPosition(0, Console.CursorTop);
+                    choice = Console.ReadLine();
+                    //
+                }
+
+            }
+        }
+    }
+}
